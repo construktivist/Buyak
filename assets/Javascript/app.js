e9d8ce9120cecb6acdf552929be157475d172fd1
Keegan Kelly <keegankelly1@gmail.com>
2016-09-18 19:09:35 -0500

    Added firebase reference

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-
+	
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzBMRn1owNGPyB24mOo6UUP0v8KfUqMaQ",
+    authDomain: "buyakdb.firebaseapp.com",
+    databaseURL: "https://buyakdb.firebaseio.com",
+    storageBucket: "buyakdb.appspot.com",
+    messagingSenderId: "781575941750"
+  };
+  firebase.initializeApp(config);
 
 
 
