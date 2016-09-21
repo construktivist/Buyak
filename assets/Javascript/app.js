407edf2a2ca6b66fe7a75fc4c9fe4e5da40f7777
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-09-20 20:15:05 -0500

    on click function

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 
+//ARRAY OF THE PRODUCTS THAT HAVE BEEN SEARCHED. THIS WILL BE USED FOR THE WISHLIST CONTENT.
+var products = [];
+
+	//WHEN SUBMIT IS CLICKED, ADD INPUT TO THE PRODUCT ARRAY
+	//WHEN SUBMIT IS CLICKED, PRODUCT MODALS WILL APPEAR
 	$('#submit').on('click', function(){
 		$('.logo').addClass('hidden');
 		$('.modal-content').removeClass('hidden');
+		var searchedItems = $('#search').val().trim();
+		products.push(searchedItems);
+		$('#search').val("");
+
+		console.log(products);
 	});
 
 
