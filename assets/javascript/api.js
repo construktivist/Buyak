2606b2ebce3c82946211dd36469d70459b9cbe09
manaliraleraskar <manali.raleraskar@gmail.com>
2016-09-24 11:51:12 -0500

    Prepend search to top and clear search text after submit

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -5,6 +5,7 @@ $("body").on("click", "#searchProduct", function() {
         
         var searchFor = $("#productSearch").val().trim();
         console.log("Value of P : " + searchFor);
+        $("#productSearch").val("");
 
         // CALL WALMART API FOR BEST SELLER PRODUCTS
         var sortQueryURLWalmart = "http://api.walmartlabs.com/v1/search?apiKey=bs4qexhbfxu9xaee8f53bhyr&query=" + searchFor +"&sort=bestseller&responseGroup=full"
@@ -34,12 +35,19 @@ $("body").on("click", "#searchProduct", function() {
                         searchResults.push(item);
                     }
 
-                    // Then dynamicaly generates button store
-                        var storeBtn = $("<button>") 
-                        storeBtn.addClass("btn btn-default animated swing store"); // Added a class 
-                        storeBtn.attr('data-store', "walmart"); // Added a data-attribute
-                        storeBtn.text("Walmart"); // Provided the initial button text
-                        $('#storeSortBtnList').append(storeBtn); // Added the button to the HTML
+                            if($('#walmart').length )  
+                            {
+                                 console.log("Best Buy Button Exist!!!");
+                                 
+                            }else{
+                                // Then dynamicaly generates button store
+                                var storeBtn = $("<button>") 
+                                storeBtn.addClass("btn btn-default animated swing store"); // Added a class 
+                                storeBtn.attr('id', "walmart");
+                                storeBtn.attr('data-store', "walmart"); // Added a data-attribute
+                                storeBtn.text("Walmart"); // Provided the initial button text
+                                $('#storeSortBtnList').append(storeBtn); // Added the button to the HTML
+                            }
     
                     // Call BESTBUY API INSIDE DONE OF WALMART...Simlarly call future api class one by one inside the done methods of call
                     var queryURLBB = "https://api.bestbuy.com/v1/products((search=" + searchFor + ")&customerReviewAverage=4.8&(categoryPath.id=abcat0101000))?apiKey=sdauhdkcw3m5f8rm3mdrqk9g&facet=onSale&pageSize=10&format=json";
@@ -71,13 +79,20 @@ $("body").on("click", "#searchProduct", function() {
                             searchResults.push(item);
                             
                           }  
-
-                          // Then dynamicaly generates button store
-                            var storeBtn = $("<button>") 
-                            storeBtn.addClass("btn btn-default animated swing store"); // Added a class 
-                            storeBtn.attr('data-store', "bestbuy"); // Added a data-attribute
-                            storeBtn.text("Best Buy"); // Provided the initial button text
-                            $('#storeSortBtnList').append(storeBtn); // Added the button to the HTML
+                            //Check if button already exist and if doesnot exist then only create a new button
+                           if($('#bestbuy').length )  
+                            {
+                                 console.log("Best Buy Button Exist!!!");
+                                 
+                            }else{
+                                // Then dynamicaly generates button store
+                                var storeBtn = $("<button>") 
+                                storeBtn.addClass("btn btn-default animated swing store"); // Added a class
+                                storeBtn.attr("id", "bestbuy");
+                                storeBtn.attr('data-store', "bestbuy"); // Added a data-attribute
+                                storeBtn.text("Best Buy"); // Provided the initial button text
+                                $('#storeSortBtnList').append(storeBtn); // Added the button to the HTML
+                            }                        
 
                            displayResults(searchResults);           
                         });
@@ -169,8 +184,8 @@ function displayResults(resultsArray){
                     divItem.append(rowProductDiv);  
                     
                     
-                    containerDiv.append(divItem);
-                    $('#productList').append(containerDiv);
+                    containerDiv.prepend(divItem);
+                    $('#productList').prepend(containerDiv);
                 }
 
 }
