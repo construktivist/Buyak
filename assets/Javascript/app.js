af4737523847e724032fca0b537b2c9baaea3a42
manaliraleraskar <manali.raleraskar@gmail.com>
2016-10-01 10:12:15 -0500

    firebase update

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -245,6 +245,7 @@ $(document).ready(function(){
        console.log("Inside Add item to wishlist click Item From array" + JSON.stringify(arrayItem));
 
         wishArray.push(arrayItem);
+
         localStorage.clear();
         localStorage.setItem("localWishlist", JSON.stringify(wishArray));
         wishItemCount++;
@@ -304,6 +305,8 @@ $(document).ready(function(){
       name : listName,
       wishlist: wishArray
       });
+
+
     });
 
 });//document.ready ends
@@ -660,6 +663,8 @@ if(JSON.parse(localStorage.getItem("localWishlist"))){
 
   displayList(wishArray);  
 
+}else{
+  $("#wishListGridDisplay").addClass("hidden");
 }
 
 
